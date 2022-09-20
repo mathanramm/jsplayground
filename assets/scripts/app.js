f2f2b3a682850b15429fd5c0d5849941fc94727c
mathanramm <meetmemathanram@yahoo.com>
2022-09-20 23:20:08 +0530

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,21 @@
-function keypress(btnpay) {
-  const press = document.getElementById(btnpay);
-  if (press.style.display === "none") {
-    press.style.display = "block";
+//  function keypress(btnpay) {
+//   const press = document.getElementById(btnpay);
+//   if (press.style.display === "none") {
+//     press.style.display = "block";
+//   } else {
+//     press.style.display = "none";
+//   }
+// }
+
+var btn = document.getElementById("btnpayy");
+var list = document.getElementById("condisp");
+
+btn.addEventListener("click", func);
+
+function func(list) {
+  if (list.style.display === "none") {
+    list.style.display = "block";
   } else {
-    press.style.display = "none";
+    list.style.display = "none";
   }
 }
-
-var a = getElementById(btnpay1);
-a.onclick = keypress1();
